98de6f6acba20455fe989db9a61ea2877b4f4bca
Ducky <ducky.vexton@outlook.com>
2017-09-16 02:48:16 +0100

    Move location of package configs

---
+++ pkg/choco/traydio/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= $env:ChocolateyPackageName
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://get.zyr.io/dist/Traydio/Win32/0.3.0/x86/Traydio-Win32-0.3.0-x86.install.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'Traydio'
+
+  checksum      = '5bb46003227630459c9bccd750130f5c'
+  checksumType  = 'md5'
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
